7069d1a54dfe7a9774d0fc4eab844984893a3ddf
Juan Pedro Alvarez <78662994+juanpeee@users.noreply.github.com>
2023-11-06 11:52:43 -0300

    Add files via upload

---
+++ Obligatoorio 2/src/dominio/Postulante.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dominio;
+
+/**
+ *
+ * @author Juan Pedro
+ */
+public class Postulante {
+    
+    int nombre;
+    int cedula;
+    String direccion;
+    int telefono;
+    String msil;
+    String linkedin;
+    
+    Postulante(int nombre,int cedula,String direccion,int telefono,String msil;String linkedin;){
+   }    
+}
